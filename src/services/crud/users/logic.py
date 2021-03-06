a4c785c0c913c31eb832e3db92af5f65954cbf11
Dinh_Ngoc <bvngoc2000@gmail.com>
2021-03-06 22:24:44 +0700

    logic for user

--- src/services/crud/users/logic.py
+++ src/services/crud/users/logic.py
@@ -1,12 +1,43 @@
 from .models import User
-from src.libs.data_manager import DataManager
+# from src.libs.data_manager import DataManager
 
 
-def check_user_exist(username: str):
+def check_user_exist(username):
+    user = User.objects(username=username).first()
+    if user:
+        return True
     return False
 
 
-def save_user(data_manager: DataManager, username, password):
-    user = User()
-    data_manager.save(user)
+def save_user(username, password):
+    user = User(username=username, password=password)
+    user.save()
+
+    return True
+
+
+def get_user_profile(username):
+    user = User.objects(username=username).first()
+
+    return user.to_json()
+
+
+def update_current_user(username, password, avatar, name):
+    user = User.objects(username=username)
+    fileds = {
+        'username': username,
+        'password': password,
+        'avatar': avatar,
+        'name': name,
+    }
+    user.update(**fileds)
+    user.reload()
+
+    return True
+
+
+def delete_current_user(username):
+    user = User.objects(username=username)
+    user.delete()
+
     return True
