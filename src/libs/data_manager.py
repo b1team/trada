248b578e434332406ea3691fa14e67089dc927cb
Dinh_Ngoc <bvngoc2000@gmail.com>
2021-03-06 22:23:03 +0700

    test

--- src/libs/data_manager.py
+++ src/libs/data_manager.py
@@ -1,6 +1,14 @@
+from src.libs import db
+
 class DataManager:
     def save(self, model):
         print("lamf cac buoc gi do de save model")
+        return True
+    
+    def update(self, model):
+        return True
+    
+    def delete(self, model):
         return True
 
 
