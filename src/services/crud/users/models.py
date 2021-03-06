5ba949385574ff19a1427a1202832482caf1521d
Dinh_Ngoc <bvngoc2000@gmail.com>
2021-03-06 22:24:58 +0700

    model for user

--- src/services/crud/users/models.py
+++ src/services/crud/users/models.py
@@ -1,2 +1,21 @@
-class User:
-    pass
+from ..database import db
+import datetime
+
+
+class User(db.Document):
+    user_id = db.ObjectIdField(db_field='_id')
+    username = db.StringField(required=True, unique=True)
+    password = db.StringField(required=True)
+    name = db.StringField(default="")
+    avatar = db.URLField(default="https://www.default.com/")
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow)
+
+    def to_json(self):
+        return {
+                "user_id" : str(self.user_id),
+                "username" : self.username,
+                "avatar" : self.avatar,
+                "password" : self.password,
+                "name" : self.name,
+                "created_at" : self.created_at
+                }
