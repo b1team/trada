3a093f11b4ce964fe8615793c3af6b73b9db0bb6
Dinh_Ngoc <bvngoc2000@gmail.com>
2021-03-06 22:24:24 +0700

    logic for user

--- src/services/crud/users/api.py
+++ src/services/crud/users/api.py
@@ -1,11 +1,42 @@
-from .logic import check_user_exist, save_user
-from src.libs.data_manager import MongodbManager
+from .logic import (check_user_exist,
+                    save_user,
+                    update_current_user,
+                    delete_current_user,
+                    get_user_profile)
+# from src.libs.data_manager import MongodbManager
 
 
 def create_user(username: str, password: str):
     user_exist = check_user_exist(username)
     if not user_exist:
-        mongo_manager = MongodbManager()
-        user_id = save_user(mongo_manager, username, password)
-        return user_id
-    return None
+        new_user = save_user(username, password)
+        return new_user
+
+    return False
+
+
+def get_user(username: str):
+    user_exist = check_user_exist(username)
+    if user_exist:
+        user = get_user_profile(username)
+        return user
+
+    return False
+
+
+def update_user(username: str, password: str, avatar: str, name: str):
+    user_exist = check_user_exist(username)
+    if user_exist:
+        user_update = update_current_user(username, password, avatar, name)
+        return user_update
+
+    return False
+
+
+def delete_user(username: str):
+    user_exist = check_user_exist(username)
+    if user_exist:
+        delete_user = delete_current_user(username)
+        return delete_user
+
+    return False
