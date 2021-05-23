d7c0df4e4fc09d0ad8f6d2aea99cdf1236c0d0f8
Dinh Ngoc <bvngoc2000@gmail.com>
2021-05-23 10:55:56 +0700

    fix port

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import uvicorn
 if __name__ == "__main__":
     options = {
         "host": "0.0.0.0",
-        "port": 9000,
+        "port": 9999,
         "log_level": "info",
         "reload": True,
     }
